ba58819e0daec3eb562079826a7892c2059e8ae9
Chiksinafees <nafeesahmed01.na@gmail.com>
2023-08-14 16:40:59 +0530

    started learning how to use MYSQL

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const deleteController = require("./controllers/delete");
 
+const db = require('./util/database')
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,6 +19,7 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+db.execute('SELECT * FROM products')
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(deleteController.delete);
